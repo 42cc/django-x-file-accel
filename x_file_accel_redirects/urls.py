7e56eaa5c40dbe2376f9fb84391db743e87a6e14
Misha Padalka <misha.cn.ua@gmail.com>
2015-12-26 17:53:08 +0200

    Django >1.4 compat

--- x_file_accel_redirects/urls.py
+++ x_file_accel_redirects/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:  # Django < 1.4
+    from django.conf.urls.defaults import patterns, url
 
 from x_file_accel_redirects import views
 
